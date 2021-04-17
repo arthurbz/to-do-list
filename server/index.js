ef9e593b9caf8014df31e02b0aec1fff6568669f
arthurbz <arthurbziero@gmail.com>
2021-04-17 15:34:35 -0300

    creating all routes for insert/update/delete

--- server/index.js
+++ server/index.js
@@ -15,12 +15,13 @@ app.post("/todos", async (req, res) => {
         const { description } = req.body;
         const newTodo = await pool.query(
             "INSERT INTO todo (description) VALUES ($1) RETURNING *",
-            [description])
+            [description]
+        );
         console.log(req.body);
 
-        res.json(newTodo.rows[0])
+        res.json(newTodo.rows[0]);
     } catch (error) {
-        console.log(error.message);
+        console.error(error.message);
     }
 });
 
@@ -28,11 +29,54 @@ app.get("/todos", async (req, res) => {
     try {
         const allTodos = await pool.query(
             "SELECT id, description FROM todo"
-        )
-        console.log(allTodos.rows)
-        res.json(allTodos.rows)
+        );
+        console.log(allTodos.rows);
+        res.json(allTodos.rows);
 
     } catch (error) {
-        console.log(error.message)
+        console.error(error.message);
     }
-})
+});
+
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query(
+            "SELECT id, description FROM todo WHERE id = ($1)",
+            [id]
+        );
+        res.json(todo.rows);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        await pool.query(
+            "UPDATE todo SET description = ($1) WHERE id = ($2)",
+            [description, id]
+        );
+        res.json("The todo with ID " + id + " was updated!");
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query(
+            "DELETE FROM todo where id = ($1)",
+            [id]
+        );
+        res.json("Task was deleted!");
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
